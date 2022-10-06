cb838d6344fc86c0c18487fbe40018a9de2f9948
vitekartanaji <tvitekar@outlook.com>
2022-10-06 14:47:57 +0530

    01_assignment
    01_assignment

---
+++ 01_assignment.js
@@ -0,0 +1,76 @@
+var myNumber;
+myNumber="100";
+console.log(myNumber);
+
+var temp;
+temp="200";                         
+temp="Javascript"
+console.log(temp);
+
+// ------------------------------------------------
+
+var my_friend_name="jenny";
+var your_friend_name="bill";
+console.log("Before swapping-");
+console.log(my_friend_name,your_friend_name);
+
+var swapped_name=my_friend_name;
+my_friend_name=your_friend_name;
+your_friend_name=swapped_name;
+console.log("After swapping-")
+console.log(my_friend_name,your_friend_name);
+
+// ---------------------------------------------------------
+
+// ------Swapping 3 variables with each other---------------
+
+var a="10";
+var b="20";
+var c="30";
+console.log("Before swapping-");
+console.log(a,b,c);
+
+var d=a;
+a=b;
+b=c;
+c=d;
+console.log("After swapping-");
+console.log(a,b,c);
+
+// -------------------Assignment-01------------------------------
+
+// 1-------
+
+var myFullName;
+console.log(myFullName);               //----undefined
+
+myFullName="Tanaji Vitekar";
+console.log(myFullName);               //-----Tanaji Vitekar
+
+myFullName="Tanaji Pralhad Vitekar";
+console.log(myFullName);               //-----Tanaji Pralhad Vitekar
+
+// 2-------
+
+var myLove="I love only JS";
+console.log(myLove);
+
+// 3-------
+
+var text1="Tanaji";
+
+var text2="Vitekar";
+
+var age=23;
+
+var address="Sangola";
+
+var pin_code=413314;
+
+var gender="M";
+
+var is_married="No";
+
+console.log(text1,text2,age,address,pin_code,gender,is_married)
+
+//-------------------------------------------------------------------
