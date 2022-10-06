57608309782e7a03ba357cdf626c8ba52829eac4
vitekartanaji <tvitekar@outlook.com>
2022-10-06 14:48:17 +0530

    swap 2/3 numbers
    swap 2/3 numbers

---
+++ 01_swap_variables.js
@@ -0,0 +1,28 @@
+// ------Swapping 2 variables------------------------------
+var a="10";
+var b="20";
+console.log("Before swap-")
+console.log(a,b)
+
+var c=a;
+a=b;
+b=c;
+console.log("After swap-")
+console.log(a,b);
+
+// ------Swapping 3 variables with each other---------------
+
+var a="10";
+var b="20";
+var c="30";
+console.log("Before swapping-");
+console.log(a,b,c);
+
+var d=a;
+a=b;
+b=c;
+c=d;
+console.log("After swapping-");
+console.log(a,b,c);
+
+// -----------------------------------------------------
