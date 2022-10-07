ba1b984aeaafc85adfd9dac06f9a8585c8e2e626
vitekartanaji <tvitekar@outlook.com>
2022-10-07 11:43:12 +0530

    Datatypes demo-null,undefined,string,number,boolen
    Datatypes demo-null,undefined,string,number,boolen

---
+++ 02_datatypes.js
@@ -0,0 +1,36 @@
+var  p_name="tanaji";
+var age=23;
+var city="pune";
+var married_status=false;
+
+console.log(typeof p_name);
+console.log(typeof age);
+console.log(typeof city);
+console.log(typeof married_status);
+
+console.log(typeof 'hello');
+console.log(typeof 101);
+console.log(typeof true);
+
+var new_var;
+console.log(new_var);
+console.log("---------------");
+console.log(typeof new_var);
+
+var data_type=null;
+console.log(typeof data_type);
+
+// -------------------------------------------------------------------
+// 01--------------
+console.log("--------------------------------------------------------------------------------");
+var type1;
+type1="Tanaji";
+console.log("Name:",type1,",","Type:",typeof type1)
+
+var type2;
+type2=101;
+console.log("Pin:",type2, ",","Type:",typeof type2)
+
+var type3;
+type3=true;
+console.log("T/F:",type3,",","Type:",typeof type3)
