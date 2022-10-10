846f2d77332042bdadaf46c39fad673440f58243
vitekartanaji <tvitekar@outlook.com>
2022-10-10 14:04:46 +0530

    03 function examples
    03 function examples

---
+++ 03_function_exp.js
@@ -0,0 +1,14 @@
+var abc=function(arg1){
+    console.log("hello",arg1)
+    return "ok"
+};
+
+var bc=abc(10);
+console.log(bc);
+//========================================================
+
+var  bc="Welcome";
+
+console.log(bc);
+
+//========================================================
---
+++ 03_functions.js
@@ -0,0 +1,7 @@
+function function_name(a1,a2){
+    console.log(a1);
+    console.log(a2);
+    // console.log(a3);
+};
+function_name(10,20,30);
+// console.log(a);
