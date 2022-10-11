59f3a19e3d6c85f71104e8bdaa9db979d51a1747
vitekartanaji <tvitekar@outlook.com>
2022-10-11 20:14:32 +0530

    04_assignment01
    04_assignment01

---
+++ 04_assignment01.js
@@ -0,0 +1,18 @@
+console.log("========================")
+
+var str1 = "Mastercard";
+console.log(`01-My dream company is-${str1}`);
+
+console.log("========================")
+
+function stringAssignment1() {
+    var hob_1="Cricket";
+    var hob_2="Football";
+    var hob_3="Movies";
+    console.log("2.1-My hobbies: ",hob_1,hob_2,hob_3);
+    console.log(`2.2-My hobbies:  ${hob_1.concat(hob_2.concat(hob_3))}`);
+}
+
+stringAssignment1();
+
+
