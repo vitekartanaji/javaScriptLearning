c8f67435c7b63bd9d9c22a2444798d7700a203d5
vitekartanaji <tvitekar@outlook.com>
2022-10-14 16:05:52 +0530

    07_array_basics_examples
    07_array_basics_examples

---
+++ 07_array_basics.js
@@ -0,0 +1,10 @@
+var array_new=[1,2,3,4,6,2,4,8];
+console.log(array_new);
+var arrayLength=array_new.length;
+console.log(`Length of an array: ${arrayLength}`);
+
+var a=array_new[0];
+console.log(a);
+
+array_new[2]=55;
+console.log(array_new);
