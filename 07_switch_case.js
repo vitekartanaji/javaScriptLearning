e10bf9128b5af7c8ab7d20037977c823d0680607
vitekartanaji <tvitekar@outlook.com>
2022-10-14 16:05:30 +0530

    07_switch_case_examples
    07_switch_case_examples

---
+++ 07_switch_case.js
@@ -0,0 +1,27 @@
+var dayNumber = 7;
+switch (dayNumber) {
+    case 1:
+        console.log("Monday")
+        break;
+    case 2:
+        console.log("Tuesday")
+        break;
+    case 3:
+        console.log("Wednesday")
+        break;
+    case 4:
+        console.log("Thursday")
+        break;
+    case 5:
+        console.log("Friday")
+        break;
+    case 6:
+        console.log("Saturday")
+        break;
+    case 7:
+        console.log("Sunday")
+        break;
+    default:
+        console.log("Invalid Input")
+        break;
+}
