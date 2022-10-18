d2b4594e4e540d9e69b58f00746dae8f41113739
vitekartanaji <tvitekar@outlook.com>
2022-10-18 21:38:31 +0530

    09_while_loop_examples
    09_while_loop_examples

---
+++ 09_while_loop.js
@@ -0,0 +1,23 @@
+var count=0;
+while (count<=10) {
+    console.log(count);
+    count++;
+}
+
+var count=5;
+while (count<=50) {
+    console.log(count);
+    count=count+5;
+}
+console.log(`-----------------------------`)
+
+
+// factorial of a number
+
+let count1=5;
+let fact=1;
+while (count1>=1) {
+    fact=fact*count1;
+    count1--;
+}
+console.log(fact);
