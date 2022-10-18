1d2ea3d007dccc367bed01faf6b89afc063d1571
vitekartanaji <tvitekar@outlook.com>
2022-10-18 21:01:18 +0530

    08_break_statements
    08_break_statements

---
+++ 08_break_continue.js
@@ -0,0 +1,18 @@
+console.log("====================Break=====================")
+
+for (let index = 0; index <=5; index++) {
+    if(index==2){
+        break;
+    }
+    console.log(index);
+};
+
+console.log("====================Continue=====================")
+
+for (let index = 0; index <=5; index++) {
+    if(index==2){
+        continue;
+    }
+    console.log(index);
+};
+
