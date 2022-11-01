d529f0d9224013d1ad889d61cc3853c66990cb09
vitekartanaji <tvitekar@outlook.com>
2022-11-01 11:48:46 +0530

    15_array_objects
    15_array_objects

---
+++ 15_array_objects.js
@@ -0,0 +1,27 @@
+class Person{
+
+    constructor(fullName, city, age, gender) {
+        this.fullName=fullName;
+        this.city=city;
+        this.age=age;
+        this.gender=gender
+    }
+    details(){
+        console.log(`Person Details: ${this.fullName} ${this.city} ${this.age} ${this.gender}`);
+    }
+};
+
+let viratPerson = new Person("Virat Kohli", "Delhi", 32, "M");
+let rohitPerson = new Person("Rohit Sharma", "Mumbai", 33, "M");
+let rishabhPerson = new Person("Rishabh Pant", "Delhi", 28, "M");
+
+const ary = [viratPerson,rohitPerson,rishabhPerson];
+
+for (let index = 0; index < ary.length; index++) {
+    const element = ary[index];
+    if (element.age>30) {
+        console.log(`${element.fullName}, ${element.city}, ${element.age}, ${element.gender}`);        
+    }
+};
+
+
