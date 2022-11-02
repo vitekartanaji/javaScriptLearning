795ea268b0f1729ea4ae8ea83894f481f08221df
vitekartanaji <tvitekar@outlook.com>
2022-11-02 13:06:58 +0530

    updated 15_array_resize
    updated 15_array_resize

---
+++ 15_array_resize.js
@@ -0,0 +1,18 @@
+let ary = [1,2,3,4,5];
+console.log(ary.length);
+console.log(ary);
+
+// resizing array by using length property
+
+ary.length = 3;
+console.log(ary.length);
+console.log(ary);
+
+ary.length = 10;
+console.log(ary.length);
+console.log(ary);
+
+// to empty an array
+
+ary.length=0;
+console.log(ary);
