95e1e1a4cad5b316065f391068ccb3dfaebec1ae
vitekartanaji <tvitekar@outlook.com>
2022-11-03 17:10:20 +0530

    17_hosting
    17_hosting

---
+++ 17_hosting.js
@@ -0,0 +1,16 @@
+// variable hosting (variable always be hosted)
+
+console.log(myName);
+var myName = "Tanaji";
+
+//----------------------------------------------
+// function hosting (simple function is always hosted)
+
+myname();
+
+function  myname(){
+    console.log("Hello")
+};
+
+
+// function expression/let/const cannot hosted
