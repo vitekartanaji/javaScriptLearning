bf9d12d9e3ddd05560de323506573df2409e2ef2
vitekartanaji <tvitekar@outlook.com>
2022-11-03 17:12:45 +0530

    18_promise
    18_promise

---
+++ 18_promise.js
@@ -0,0 +1,17 @@
+let is_sir_shared_notes = false;
+
+let promice = new Promise(function(resolve, rejected){
+    if(is_sir_shared_notes){
+        resolve("Sir shared the notes.");
+    }else{
+        rejected("Sir did not shared the notes.");
+    }
+});
+
+promice.then(function(Success){
+    console.log(Success);
+}).catch(function(Failure){
+    console.log(Failure);
+}).finally(function(Thank_You){
+    console.log("Thank you.");
+})
