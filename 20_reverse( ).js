89636a9f6a84564c03c7a1e79ad2a4ed895c17d9
vitekartanaji <tvitekar@outlook.com>
2022-11-26 12:38:37 +0530

    20_reverse
    20_reverse

---
+++ 20_reverse( ).js
@@ -0,0 +1,8 @@
+const arrayOfNumbers = [100,21,300,4,8,47,67,5];
+const arrayOfNames = ["one","two","three","four","five"];
+
+arrayOfNumbers.reverse();
+console.log(arrayOfNumbers);
+
+arrayOfNames.reverse();
+console.log(arrayOfNames);
